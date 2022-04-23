3a066e911b17d0cda862a43a3ff130a87788d362
David Samuelson <linksn.1fan@gmail.com>
2022-04-23 16:11:05 -0600

    replaced Readme

--- db/connect.js
+++ db/connect.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection(
     {
         host: 'localhost',
         user: 'root',
-        password: ' ',
+        password: 'Int3rn3t!',
         database: 'workbase'
     },
     console.log('connected to workbase')
